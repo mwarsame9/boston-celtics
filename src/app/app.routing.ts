e18688ae1783ed713af6f53a4f19962c19eb660d
Mohamed Warsame <mwarsame9@gmail.com>
2017-06-16 10:52:50 -0700

    add router

---
+++ src/app/app.routing.ts
@@ -0,0 +1,33 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { RosterComponent } from './roster/roster.component';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
+import { AdminComponent }   from './admin/admin.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'roster',
+    component: RosterComponent
+  },
+  {
+    path: 'players/:id',
+    component: PlayerDetailComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
--- src/app/player.model.ts
+++ src/app/player.model.ts
@@ -1,2 +1,3 @@
 export class Player {
+  constructor (public name: string, public position: string, public number: number) { }
 }
